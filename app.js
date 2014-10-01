f2362d3fe268c0d85ce2e9779fd911b12fe1b34c
Daniel Stepp <willstepp@gmail.com>
2014-09-30 23:50:45 -0400

    added basic auth

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var http = require('http');
 app = express();
 app.set('port', process.env.PORT || 3002);
 
+app.use(express.basicAuth(process.env.POD_USER, process.env.POD_PSWD));
+
 app.get('/', function(req, res){
   res.send('<h3>Pod</h3>');
 });
