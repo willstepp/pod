00552f5aa661ee55c113ac3edf384d12fb0b03c9
Daniel Stepp <willstepp@gmail.com>
2014-09-30 23:57:38 -0400

    changes

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var http = require('http');
 app = express();
 app.set('port', process.env.PORT || 3002);
 
-app.use(express.basicAuth(process.env.POD_USER, process.env.POD_PSWD));
+app.use(express.basicAuth(function(user, pass){
+  return process.env.POD_USER === user && process.env.POD_PSWD == pass;
+}));
 
 app.get('/', function(req, res){
   res.send('<h3>Pod</h3>');
